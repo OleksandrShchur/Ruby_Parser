5b8b1d373ac6a62e469c6aace1ec7cf98631d94a
oleksandr_shchur <osur12709@gmail.com>
2022-12-21 13:20:52 +0200

    add Zipper class

---
+++ Zipper.rb
@@ -0,0 +1,41 @@
+require 'rubygems'
+require 'zip'
+
+class Zipper
+    def initialize(input_dir, output_file)
+        @input_dir = input_dir
+        @output_file = output_file
+    end
+
+    def write
+        entries = Dir.entries(@input_dir) - %w[. ..]
+    
+        ::Zip::File.open(@output_file, create: true) do |zipfile|
+          write_entries entries, '', zipfile
+        end
+    end
+
+    def write_entries(entries, path, zipfile)
+        entries.each do |e|
+          zipfile_path = path == '' ? e : File.join(path, e)
+          disk_file_path = File.join(@input_dir, zipfile_path)
+    
+          if File.directory? disk_file_path
+            recursively_deflate_directory(disk_file_path, zipfile, zipfile_path)
+          else
+            put_into_archive(disk_file_path, zipfile, zipfile_path)
+          end
+        end
+    end
+
+    def recursively_deflate_directory(disk_file_path, zipfile, zipfile_path)
+        zipfile.mkdir zipfile_path
+        subdir = Dir.entries(disk_file_path) - %w[. ..]
+        write_entries subdir, zipfile_path, zipfile
+    end
+    
+    def put_into_archive(disk_file_path, zipfile, zipfile_path)
+        zipfile.add(zipfile_path, disk_file_path)
+    end
+
+end
